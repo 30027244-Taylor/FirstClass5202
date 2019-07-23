2dbf05198779f9b86221d6a718a450446c8689c1
Taylor Hollander <30027244@student.toiohomai.ac.nz>
2019-07-24 11:47:35 +1200

    my first commit

---
+++ App2/App2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your first name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please enter your last name: ");
+            string lname = Console.ReadLine();
+            Console.WriteLine("Hello {0} {1}, nice to meet you", name, lname);
+            Console.WriteLine("Hello" + " " + name + " " + lname + "nice to meet you");
+            Console.WriteLine("Hello {name} {lname}, nice to meet you");
+            Console.ReadLine();
+        }
+    }
+}
